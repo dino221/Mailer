0d927a215f86a14e2f94de8270d59a083b72420e
dino221 <dino.bosilj@gmail.com>
2021-01-13 14:35:11 +0100

    Initial commit

---
+++ mail.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+	$mail -> SMTPDebug = 1;
+	$mail -> isSMTP();
+	$mail -> Host = 'smtp.example.com;';
+	$mail -> SMTPAuth = true;
+	$mail -> Username = 'user@example.com';
+	$mail -> Password = 'secret';
+	$mail -> SMTPSecure = 'tls';
+	$mail ->Port = 587;
+
+	$mail -> setFrom('from@example.com', 'Mailer');
+	$mail -> addAddress('joe@example.net', 'Joe User');
+
+	$mail -> setFrom('from@example.com', 'Mailer');
+	$mail -> addAddress('joe@example.net', 'Joe User');
+
+	$mail -> isHTML(true);
+	$mail -> Subject = 'Here is the subject';
+	$mail -> Body = 'This is the HTML message body <b>in bold!</b>';
+	$mail -> AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+	$mail -> send();
+	echo 'Message has been sent';
+} catch (Exception $e) {
+	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
+	
